5090f438cd3c9b507a7bfd625627acbe66b58165
Hristo Deshev <hristo@deshev.com>
2016-04-06 13:48:24 +0300

    Detect and run webpack from the correct node_modules subdir.
    Either the project's node_modules, or the plugin subdir (if another version
    is installed by the project)

--- postinstall.js
+++ postinstall.js
@@ -6,20 +6,6 @@ var fs = require("fs");
 
 var projectDir = hook.findProjectDir();
 
-// Register webpack script in package.json.
-var packageJsonPath = path.join(projectDir, "package.json");
-var packageJsonContent = fs.readFileSync(packageJsonPath, "utf8");
-var jsonContent = JSON.parse(packageJsonContent);
-
-var scripts = jsonContent.scripts || {};
-if (!scripts.webpack) {
-    scripts.webpack = "webpack";
-}
-jsonContent.scripts = scripts;
-
-var modifiedPackageJson = JSON.stringify(jsonContent, null, 4);
-var packageJsonContent = fs.writeFileSync(packageJsonPath, modifiedPackageJson);
-
 // Create a webpack.config.js file, if not present.
 var configTemplatePath = path.join(__dirname, "webpack.config.js.template");
 var configPath = path.join(projectDir, "webpack.config.js");
