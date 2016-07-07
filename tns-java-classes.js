828a5d36f48e394e85355d40c7ccee8c63895c13
Hristo Deshev <hristo@deshev.com>
2016-07-07 15:26:23 +0300

    Update __requireOverride for android-runtime 2.1+

--- tns-java-classes.js
+++ tns-java-classes.js
@@ -10,14 +10,16 @@
 
 require("application");
 require("ui/frame");
+require("ui/frame/activity");
 
 if (global.TNS_WEBPACK) {
     global.__requireOverride = function (name, dir) {
-		var tnsModulesPrefix = "./tns_modules/";
         if (name === "./tns_modules/application/application.js") {
             return require("application");
         } else if (name === "./tns_modules/ui/frame/frame.js") {
             return require("ui/frame");
+        } else if (name === "./tns_modules/ui/frame/activity.js") {
+            return require("ui/frame/activity");
         }
     };
 }
