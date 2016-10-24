1c92563d1a4dcbc97f51fe0dcbc0090a556cff10
Hristo Deshev <hristo@deshev.com>
2016-10-24 17:01:08 +0300

    Rework bundling and trigger it with an external npm run command.
    - Install webpack config templates for both Android and iOS.
    - Allow running webpack without triggering tns builds.
    - Let users extend the bundling process by editing script commands
    in their package.json files.

--- index.js
+++ index.js
@@ -1,98 +1,23 @@
-var webpack = require("webpack");
-var ConcatSource = require("webpack/lib/ConcatSource");
-var shelljs = require("shelljs");
-var path = require("path");
+var sources = require("webpack-sources");
 var fs = require("fs");
-
-var tnsPackage = "tns-core-modules";
-var tnsModulesDir = path.join("node_modules", tnsPackage);
-
-var platform = process.env.PLATFORM;
-var platformOutDir = process.env.PLATFORM_DIR;
-
-exports.readPackageJson = function(dir) {
-    var packageJson = path.join(dir, "package.json");
-    if (shelljs.test("-f", packageJson)) {
-        return JSON.parse(shelljs.cat(packageJson));
-    } else {
-        return {};
-    }
-};
-
-exports.getPackageMain = function(packageDir) {
-    if (shelljs.test("-f", packageDir + ".js")) {
-        return packageDir + ".js";
-    }
-
-    var data = exports.readPackageJson(packageDir);
-    if (data.main) {
-        var main = data.main;
-        if (/\.js$/i.test(main)) {
-            return path.join(packageDir, main);
-        } else {
-            return path.join(packageDir, main + ".js");
-        }
-    } else {
-        var indexPath = path.join(packageDir, "index.js");
-        if (shelljs.test("-f", indexPath)) {
-            return path.join(packageDir, "index.js");
-        } else {
-            throw new Error("Main module not found for: " + packageDir);
-        }
-    }
-};
-
-exports.writePackageJson = function writePackageJson(dir, data) {
-    var packageJson = path.join(dir, "package.json");
-    fs.writeFileSync(packageJson, JSON.stringify(data, null, 4), 'utf8');
-};
-
-function endWithJs(fileName) {
-    if (/\.js$/i.test(fileName)) {
-        return fileName;
-    } else {
-        return fileName + ".js";
-    }
-}
-
-exports.getEntryPoint = function getEntryPoint(appDir) {
-    var packageJson = exports.readPackageJson(appDir);
-    var entryModule = null;
-    if (packageJson.bundleMain) {
-        entryModule = endWithJs(packageJson.bundleMain);
-    } else {
-        entryModule = exports.getPackageMain(appDir);
-    }
-
-    // Strip leading dir name and return just the submodule.
-    return entryModule.replace(/^[^\\\/]+[\\\/]/, "./");
-};
-
-exports.getBundleDestination = function getBundleDestination(appDir) {
-    var packageJson = exports.readPackageJson(appDir);
-    var bundleOutput = "bundle.js";
-    if (packageJson.bundleOutput) {
-        bundleOutput = packageJson.bundleOutput;
-    }
-    return path.join(platformOutDir, appDir, bundleOutput);
-};
+var path = require("path");
 
 //HACK: changes the JSONP chunk eval function to `global["nativescriptJsonp"]`
 // applied to tns-java-classes.js only
-function FixJsonpPlugin(options) {
+exports.NativeScriptJsonpPlugin = function(options) {
 }
 
-FixJsonpPlugin.prototype.apply = function(compiler) {
-    compiler.plugin('compilation', function(compilation, params) {
-        compilation.plugin("optimize-chunk-assets", function(chunks, callback) {
-            chunks.forEach(function(chunk) {
-                chunk.files.forEach(function(file) {
-                    if (file === "tns-java-classes.js") {
+exports.NativeScriptJsonpPlugin.prototype.apply = function (compiler) {
+    compiler.plugin('compilation', function (compilation, params) {
+        compilation.plugin("optimize-chunk-assets", function (chunks, callback) {
+            chunks.forEach(function (chunk) {
+                chunk.files.forEach(function (file) {
+                    if (file === "vendor.js") {
                         var src = compilation.assets[file];
                         var code = src.source();
                         var match = code.match(/window\["nativescriptJsonp"\]/);
                         if (match) {
-                            compilation.assets[file] = new ConcatSource(code.replace(/window\["nativescriptJsonp"\]/g,  "global[\"nativescriptJsonp\"]"));
+                            compilation.assets[file] = new sources.ConcatSource(code.replace(/window\["nativescriptJsonp"\]/g, "global[\"nativescriptJsonp\"]"));
                         }
                     }
                 });
@@ -102,66 +27,35 @@ FixJsonpPlugin.prototype.apply = function(compiler) {
     });
 };
 
+exports.GenerateBundleStarterPlugin = function(bundles) {
+    this.bundles = bundles;
+}
 
-exports.getConfig = function getConfig(userDefined) {
-    var platformOutDir = process.env.PLATFORM_DIR;
-    var appOutDir = path.join(platformOutDir, "app");
+exports.GenerateBundleStarterPlugin.prototype = {
+    apply: function (compiler) {
+        var plugin = this;
+        plugin.webpackContext = compiler.options.context;
 
-    if (!userDefined.context) {
-        userDefined.context = path.resolve("./app");
-    }
-    if (!userDefined.entry) {
-        userDefined.entry = {
-            "bundle": exports.getEntryPoint("./app"),
-        };
-        if (platform === "android") {
-            userDefined.entry["tns-java-classes"] = "./tns-java-classes";
-        }
-    }
-    if (!userDefined.output) {
-        userDefined.output = {
-            pathinfo: true,
-            path: appOutDir,
-            libraryTarget: "commonjs2",
-            filename: "[name].js",
-            jsonpFunction: "nativescriptJsonp",
-        };
-    }
-    if (!userDefined.resolve) {
-        userDefined.resolve = {};
-    }
-    if (!userDefined.resolve.extensions) {
-        userDefined.resolve.extensions = [];
-    }
-    userDefined.resolve.extensions.push("");
-    userDefined.resolve.extensions.push(".js");
-    userDefined.resolve.extensions.push("." + platform + ".js");
+        compiler.plugin('emit', function (compilation, cb) {
+            console.log(" GenerateBundleStarterPlugin: " + plugin.webpackContext);
 
-    if (!userDefined.resolve.modulesDirectories) {
-        userDefined.resolve.modulesDirectories = [];
-    }
-    userDefined.resolve.modulesDirectories.push("node_modules/tns-core-modules");
-    userDefined.resolve.modulesDirectories.push("node_modules");
+            compilation.assets["package.json"] = plugin.generatePackageJson();
+            compilation.assets["starter.js"] = plugin.generateStarterModule();
 
-    if (!userDefined.plugins) {
-        userDefined.plugins = [];
-    }
-    if (!userDefined.resolverPlugins) {
-        userDefined.resolverPlugins = [];
-    }
-    userDefined.plugins.push(
-        new webpack.DefinePlugin({
-            global: 'global',
-            __dirname: '__dirname',
-            "global.TNS_WEBPACK": 'true',
-        })
-    );
-    if (platform === "android") {
-        userDefined.plugins.push(new webpack.optimize.CommonsChunkPlugin(
-            "tns-java-classes", "tns-java-classes.js", Infinity));
-    }
+            cb();
+        });
+    },
+    generatePackageJson: function() {
+        var packageJsonPath = path.join(this.webpackContext, "package.json");
+        var packageData = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+        packageData.main = "starter";
 
-    userDefined.plugins.push(new FixJsonpPlugin());
-
-    return userDefined;
+        return new sources.RawSource(JSON.stringify(packageData, null, 4));
+    },
+    generateStarterModule: function() {
+        var moduleSource = this.bundles.map(function(bundle) {
+            return "require(\"" + bundle + "\");";
+        }).join("\n");
+        return new sources.RawSource(moduleSource);
+    },
 };
--- postinstall.js
+++ postinstall.js
@@ -1,23 +1,124 @@
-var hook = require('nativescript-hook')(__dirname);
-hook.postinstall();
-
 var path = require("path");
 var fs = require("fs");
+var child_process = require("child_process");
 
-var projectDir = hook.findProjectDir();
+var projectDir = path.dirname(path.dirname(__dirname));
 var appDir = path.join(projectDir, "app");
 
-// Create a webpack.config.js file, if not present.
-var configTemplatePath = path.join(__dirname, "webpack.config.js.template");
-var configPath = path.join(projectDir, "webpack.config.js");
-if (!fs.existsSync(configPath)) {
-    var configContent = fs.readFileSync(configTemplatePath, "utf8");
-    fs.writeFileSync(configPath, configContent);
+var packageJsonPath = path.join(projectDir, "package.json");
+var packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+
+var isAngular = Object.keys(packageJson.dependencies).filter(function(dependency) {
+    return /^@angular\b/.test(dependency);
+}).length > 0;
+
+var isTypeScript = fs.existsSync(path.join(projectDir, "tsconfig.json"));
+if (isAngular) {
+    isTypeScript = true;
 }
 
-var javaClassesSrc = path.join(__dirname, "tns-java-classes.js");
-var javaClassesDest = path.join(appDir, "tns-java-classes.js");
-if (!fs.existsSync(javaClassesDest)) {
-    var javaClassesContent = fs.readFileSync(javaClassesSrc, "utf8");
-    fs.writeFileSync(javaClassesDest, javaClassesContent);
+copyProjectTemplate("webpack.common.js.template", "webpack.common.js");
+copyProjectTemplate("webpack.android.js.template", "webpack.android.js");
+copyProjectTemplate("webpack.ios.js.template", "webpack.ios.js");
+
+if (isAngular) {
+    copyAppTemplate("vendor.ts.angular.template", tsOrJs("vendor"));
+} else {
+    copyAppTemplate("vendor.ts.nativescript.template", tsOrJs("vendor"));
+}
+
+copyAppTemplate("vendor-platform.android.ts.template", tsOrJs("vendor-platform.android"));
+copyAppTemplate("vendor-platform.ios.ts.template", tsOrJs("vendor-platform.ios"));
+
+addPlatformScript(packageJson, "clean-[PLATFORM]", "tns clean-app [PLATFORM]");
+addPlatformScript(packageJson, "prewebpack-[PLATFORM]", "npm run clean-[PLATFORM]");
+addPlatformScript(packageJson, "webpack-[PLATFORM]", "npm run clean-[PLATFORM] && webpack --config=webpack.[PLATFORM].js");
+addPlatformScript(packageJson, "prestart-[PLATFORM]", "npm run webpack-[PLATFORM]");
+addPlatformScript(packageJson, "start-[PLATFORM]", "tns run [PLATFORM] --bundle --disable-npm-install");
+addPlatformScript(packageJson, "prebuild-[PLATFORM]", "npm run webpack-[PLATFORM]");
+addPlatformScript(packageJson, "build-[PLATFORM]", "tns build [PLATFORM] --bundle --disable-npm-install");
+
+dependencies(packageJson.devDependencies, function(add) {
+    add("webpack", "~2.1.0-beta.25");
+    add("webpack-sources", "~0.1.2");
+    add("copy-webpack-plugin", "~3.0.1");
+    add("awesome-typescript-loader", "~2.2.4");
+    add("html-loader", "~0.4.3");
+});
+
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 4));
+
+function addPlatformScript(packageJson, nameTemplate, commandTemplate) {
+    if (!packageJson.scripts) {
+        packageJson.scripts = {};
+    }
+
+    var scripts = packageJson.scripts;
+    ["android", "ios"].forEach(function(platform) {
+        var name = nameTemplate.replace(/\[PLATFORM\]/g, platform);
+        var command = commandTemplate.replace(/\[PLATFORM\]/g, platform);
+        if (!scripts[name]) {
+            scripts[name] = command;
+            console.log("Registering script: " + name);
+        }
+    });
+}
+
+function dependencies(dependencies, adderCallback) {
+    var pendingNpmInstall = false;
+
+    adderCallback(function(name, version) {
+        if (!dependencies[name]) {
+            dependencies[name] = version;
+            console.info("Adding dev dependency: " + name + "@" + version);
+            pendingNpmInstall = true;
+        } else {
+            console.info("Dev dependency: '" + name + "' already added. Leaving version: " + dependencies[name]);
+        }
+    });
+
+    if (pendingNpmInstall) {
+        console.info("Installing new dependencies...");
+        //Run `npm install` after everything else.
+        setTimeout(function() {
+            var spawnArgs = [];
+            if (/^win/.test(process.platform)) {
+                spawnArgs = ["cmd.exe", ["/c", "npm", "install"]];
+            } else {
+                spawnArgs = ["npm", ["install"]];
+            }
+            spawnArgs.push({ cwd: projectDir, stdio: "inherit" });
+            var npm = child_process.spawn.apply(null, spawnArgs);
+            npm.on('close', function(code) {
+                process.exit(code);
+            });
+        }, 100);
+    }
+}
+
+function tsOrJs(name) {
+    if (isTypeScript) {
+        return name + ".ts";
+    } else {
+        return name + ".js";
+    }
+}
+
+function copyProjectTemplate(templateName, projectPath) {
+    var destinationPath = path.join(projectDir, projectPath);
+    copyTemplate(templateName, destinationPath);
+}
+
+function copyAppTemplate(templateName, appPath) {
+    var destinationPath = path.join(appDir, appPath);
+    copyTemplate(templateName, destinationPath);
+}
+
+function copyTemplate(templateName, destinationPath) {
+    var templatePath = path.join(__dirname, templateName);
+    // Create destination file, only if not present.
+    if (!fs.existsSync(destinationPath)) {
+        var content = fs.readFileSync(templatePath, "utf8");
+        fs.writeFileSync(destinationPath, content);
+    }
 }
