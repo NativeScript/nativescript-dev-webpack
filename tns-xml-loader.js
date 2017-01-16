5e99f9cc8622cd40dbe45838cc213b644d8ed0f2
sis0k0 <stanimira.vlaeva@gmail.com>
2017-01-16 16:35:11 +0200

    fix: register elements from embedded templates
    Make the tns-xml-loader parse Angular templates from TypeScript files.
    
    fixes #55

--- tns-xml-loader.js
+++ tns-xml-loader.js
@@ -20,25 +20,59 @@ if (!global[ELEMENT_REGISTRY]) {
     };
 }
 
-module.exports = function (source, map) {
+function parseResource(source, map) {
     this.cacheable();
 
-    var loader = this;
+    let templateSource;
+    try {
+        templateSource = getTemplateSource(this.resourcePath, source);
+    } catch(e) {
+        this.emitWarning(e.message);
+        return this.callback(null, source, map);
+    }
+
+    if (templateSource === "") {
+        return this.callback(null, source, map);
+    }
 
     var parser = new htmlparser.Parser({
         onopentag: function (name, attribs) {
             // kebab-case to CamelCase
             var elementName = name.split("-").map(function (s) { return s[0].toUpperCase() + s.substring(1); }).join("");
+
             // Module path from element name
             var modulePath = MODULES[elementName] || UI_PATH +
                 (elementName.toLowerCase().indexOf("layout") !== -1 ? "layouts/" : "") +
                 elementName.split(/(?=[A-Z])/).join("-").toLowerCase();
+
             // Update ELEMENT_REGISTRY
             global[ELEMENT_REGISTRY][modulePath] = elementName;
         }
     }, { decodeEntities: true, lowerCaseTags: false });
-    parser.write(source);
+
+    parser.write(templateSource);
     parser.end();
 
-    this.callback(null, source, map);
-};
+    return this.callback(null, source, map);
+}
+
+function getTemplateSource(path, source) {
+    if (isTemplate(path)) {
+        return source;
+    } else if (isComponent(path)) {
+        const templateMatcher = /template\s*:\s*([`'"])((.|\n)*?)\1/;
+        return templateMatcher.test(source) ? source.replace(templateMatcher, "$2") : "";
+    } else {
+        throw new Error(`The NativeScript XML loader must be used with HTML, XML or TypeScript files`);
+    }
+}
+
+function isComponent(resource) {
+    return /\.ts$/i.test(resource);
+}
+
+function isTemplate(resource) {
+    return /\.html$|\.xml$/i.test(resource);
+}
+
+module.exports = parseResource;
