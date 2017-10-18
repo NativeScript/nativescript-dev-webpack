66d77c1eadbb61c5ca4dc30918292a541afacf2b
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2017-10-18 10:42:23 +0300

    chore: provide --force flag to update webpack configurations (#295)

--- projectFilesManager.js
+++ projectFilesManager.js
@@ -62,7 +62,7 @@ function removeProjectFiles(projectDir, appDir) {
 function deleteFile(destinationPath) {
     if (fs.existsSync(destinationPath)) {
         console.info(`Deleting file: ${destinationPath}`);
-        fs.unlink(destinationPath);
+        fs.unlinkSync(destinationPath);
     }
 }
 
