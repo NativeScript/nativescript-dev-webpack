df12dfc17741f3f72b3a14a17b47f465ad4fbc38
Panayot Cankov <Panayot.Cankov@telerik.com>
2017-11-09 10:51:11 +0200

    Fix merge conflict

--- plugins/index.js
+++ plugins/index.js
@@ -3,5 +3,5 @@ module.exports = Object.assign({},
     require("./NativeScriptJsonpPlugin"),
     require("./NativeScriptSnapshotPlugin"),
     require("./PlatformSuffixPlugin"),
-    require("./PlatformFSPlugin"),
+    require("./PlatformFSPlugin")
 );
