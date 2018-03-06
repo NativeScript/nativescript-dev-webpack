bbeb798e24585064ef77db6a3e1919e6b27fc7aa
sis0k0 <stanimira.vlaeva@gmail.com>
2018-03-06 15:42:09 +0200

    fix-next: ignore watching of hidden files

--- templates/webpack.typescript.js
+++ templates/webpack.typescript.js
@@ -19,7 +19,12 @@ module.exports = env => {
     const config = {
         context: resolve("./app"),
         watchOptions: {
-            ignored: resolve("./app/App_Resources")
+            ignored: [
+                resolve("./app/App_Resources"),
+                // Don't watch hidden files
+                "**/.*",
+                ".*",
+            ]
         },
         target: nativescriptTarget,
         entry: {
