NativeScript/nativescript-dev-webpack

commit d8a17a6fafd8c7d7ae9bf79122d012d4d18bc9c7

fix: remove mangle excludes (#461)
BREAKING CHANGES:

`uglifyMangleExcludes` is no longer exported from the
nativescript-dev-webpack plugin and shouldn't be used in the webpack
configuration.

Before:
```
// webpack.config.js
// ...
uglifyOptions: {
    mangle: { reserved: nsWebpack.uglifyMangleExcludes },
    // ...
}

```

After:
```
// webpack.config.js
// ...
uglifyOptions: {
    // ...
}

```

index.js
@@ -12,8 +12,6 @@ if (isAngular({ projectDir: PROJECT_DIR })) {
     Object.assign(exports, require('./plugins/angular'));
 }
 
-exports.uglifyMangleExcludes = require("./mangle-excludes");
-
 exports.getEntryModule = function () {
     const maybePackageJsonEntry = getPackageJsonEntry();
     if (!maybePackageJsonEntry) {

mangle-excludes.js
@@ -1,141 +1,0 @@
-module.exports = [
-    // Control names
-    "AbsoluteLayout",
-    "ActionBar",
-    "ActivityIndicator",
-    "Button",
-    "DatePicker",
-    "DockLayout",
-    "EditableTextBase",
-    "FlexboxLayout",
-    "GridLayout",
-    "Image",
-    "Label",
-    "Layout",
-    "LayoutBase",
-    "ListPicker",
-    "ListView",
-    "Page",
-    "Progress",
-    "SearchBar",
-    "SegmentedBar",
-    "Slider",
-    "StackLayout",
-    "Switch",
-    "TabView",
-    "TextBase",
-    "TextField",
-    "TextView",
-    "TimePicker",
-    "View",
-    "WrapLayout",
-
-    // Android native class extenders
-    "BroadcastReceiver",
-    "CustomTypefaceSpan",
-    "DialogFragmentClassInner",
-    "FragmentClass",
-    "ListViewAdapter",
-    "LruBitmapCache",
-    "NativeScriptActivity",
-    "OurTabHost",
-    "PageChangedListener",
-    "PagerAdapterClassInner",
-    "PinchGestureListener",
-    "SegmentedBarColorDrawable",
-    "SwipeGestureListener",
-    "TapAndDoubleTapGestureListener",
-    "WebViewClientClassInner",
-
-    // tns 3.0
-    "DisableUserInteractionListener",
-    "EditorActionListener",
-    "FocusChangeListener",
-    "TextWatcher",
-    "ValueChangeListenerImpl",
-
-    "AnimationListnerImpl",
-    "AnimationListenerImpl",
-    "TransitionListenerImpl",
-    "CheckedChangeListenerImpl",
-    "ClickListenerImpl",
-    "CompatCloseListenerImpl",
-    "CloseListenerImpl",
-    "DateChangedListenerImpl",
-    "DialogFragmentImpl",
-    "EditTextListenersImpl",
-    "FormatterImpl",
-    "ImageLoadedListenerImpl",
-    "ItemClickListenerImpl",
-    "MenuItemClickListenerImpl",
-    "NSCacheDelegateImpl",
-    "NativeViewGroupImpl",
-    "PageChangedListenerImpl",
-    "PagerAdapterImpl",
-    "PinchGestureListenerImpl",
-    "CompatQueryTextListenerImpl",
-    "QueryTextListenerImpl",
-    "SeekBarChangeListenerImpl",
-    "SwipeGestureListenerImpl",
-    "TabChangeListenerImpl",
-    "TabContentFactoryImpl",
-    "TabHostImpl",
-    "TapAndDoubleTapGestureListenerImpl",
-    "TextTransformationImpl",
-    "TimeChangedListenerImpl",
-    "TouchListenerImpl",
-    "WebViewClientImpl",
-
-    // iOS native class extenders
-    "AnimatedTransitioning",
-    "AnimationDelegateImpl",
-    "DataSource",
-    "FrameHandlerImpl",
-    "ListPickerDataSource",
-    "ListPickerDelegateImpl",
-    "ListViewCell",
-    "LocationListenerImpl",
-    "NSURLSessionTaskDelegateImpl",
-    "NotificationObserver",
-    "ObserverClass",
-    "Responder",
-    "SelectionHandlerImpl",
-    "SliderChangeHandlerImpl",
-    "SwitchChangeHandlerImpl",
-    "TapBarItemHandlerImpl",
-    "TapHandlerImpl",
-    "TimerTargetImpl",
-    "TouchGestureRecognizer",
-    "TransitionDelegate",
-    "UIActionSheetDelegateImpl",
-    "UIAlertViewDelegateImpl",
-    "UIDatePickerChangeHandlerImpl",
-    "UIDocumentInteractionControllerDelegateImpl",
-    "UIGestureRecognizerDelegateImpl",
-    "UIGestureRecognizerImpl",
-    "UIImagePickerControllerDelegateImpl",
-    "UINavigationControllerAnimatedDelegate",
-    "UINavigationControllerDelegateImpl",
-    "UINavigationControllerImpl",
-    "UIScrollViewDelegateImpl",
-    "UISearchBarDelegateImpl",
-    "UITabBarControllerDelegateImpl",
-    "UITabBarControllerImpl",
-    "UITableViewDelegateImpl",
-    "UITableViewRowHeightDelegateImpl",
-    "UITextFieldDelegateImpl",
-    "UITextFieldImpl",
-    "UITextViewDelegateImpl",
-    "UITimePickerChangeHandlerImpl",
-    "UIViewControllerImpl",
-    "UIWebViewDelegateImpl",
-    "Window",
-
-    // Sidedrawer transitions
-    // Should be removed after {N} 4.0 is released
-    // See: https://github.com/telerik/nativescript-ui-feedback/issues/477#issuecomment-360772046
-    "PushTransition",
-    "FadeTransition",
-    "SlideInOnTopTransition",
-];
-

templates/webpack.angular.js
@@ -161,7 +161,6 @@ module.exports = env => {
         const compress = platform !== "android";
         config.plugins.push(new UglifyJsPlugin({
             uglifyOptions: {
-                mangle: { reserved: nsWebpack.uglifyMangleExcludes },
                 compress,
             }
         }));

templates/webpack.javascript.js
@@ -139,7 +139,6 @@ module.exports = env => {
         const compress = platform !== "android";
         config.plugins.push(new UglifyJsPlugin({
             uglifyOptions: {
-                mangle: { reserved: nsWebpack.uglifyMangleExcludes },
                 compress,
             }
         }));

templates/webpack.typescript.js
@@ -141,7 +141,6 @@ module.exports = env => {
         const compress = platform !== "android";
         config.plugins.push(new UglifyJsPlugin({
             uglifyOptions: {
-                mangle: { reserved: nsWebpack.uglifyMangleExcludes },
                 compress,
             }
         }));

commit 7015346959f6babb049277b14ecc48044a0aafb2

fix-next: export uglifyMangleExcludes for backwards compatibility (#470)
reverts the breaking change caused by #461

index.js
@@ -33,6 +33,9 @@ exports.getAotEntryModule = function (appDirectory = APP_PATH) {
     return aotEntry;
 }
 
+// Exported for backwards compatibility
+exports.uglifyMangleExcludes = [];
+
 exports.getEntryModule = function (appDirectory = APP_PATH) {
     const entry = getPackageJsonEntry(appDirectory);
 