1e52094a6ff6d1b0e42b66cd7fa79891045b16a6
sis0k0 <stanimira.vlaeva@gmail.com>
2018-03-26 21:03:16 +0300

    refactor: remove old update scripts

--- projectFilesManager.js
+++ projectFilesManager.js
@@ -3,33 +3,6 @@ const fs = require("fs");
 
 const { isTypeScript, isAngular } = require("./projectHelpers");
 
-const FRAME_MATCH = /(\s*)(require\("ui\/frame"\);)(\s*)(require\("ui\/frame\/activity"\);)/g;
-const SCOPED_FRAME = `if (!global["__snapshot"]) {
-    // In case snapshot generation is enabled these modules will get into the bundle
-    // but will not be required/evaluated.
-    // The snapshot webpack plugin will add them to the tns-java-classes.js bundle file.
-    // This way, they will be evaluated on app start as early as possible.
-$1    $2$3    $4
-}`;
-
-const CONFIG_MATCH = /(exports = [^]+?)\s*return ({[^]+target:\s*nativescriptTarget[^]+?};)/;
-const CONFIG_REPLACE = `$1
-
-    const config = $2
-
-    if (env.snapshot) {
-        plugins.push(new nsWebpack.NativeScriptSnapshotPlugin({
-            chunk: "vendor",
-            projectRoot: __dirname,
-            webpackConfig: config,
-            targetArchs: ["arm", "arm64", "ia32"],
-            tnsJavaClassesOptions: { packages: ["tns-core-modules" ] },
-            useLibs: false
-        }));
-    }
-
-    return config;`;
-
 function addProjectFiles(projectDir, appDir) {
     const projectTemplates = getProjectTemplates(projectDir);
     Object.keys(projectTemplates).forEach(function(templateName) {
@@ -122,65 +95,6 @@ function getFullTemplatesPath(projectDir, templates) {
     return updatedTemplates;
 }
 
-function editExistingProjectFiles(projectDir) {
-    const webpackConfigPath = getFullPath(projectDir, "webpack.config.js");
-    const webpackCommonPath = getFullPath(projectDir, "webpack.common.js");
-
-    const configChangeFunctions = [
-        replaceStyleUrlResolvePlugin,
-        addSnapshotPlugin,
-    ];
-
-    editFileContent(webpackConfigPath, ...configChangeFunctions);
-    editFileContent(webpackCommonPath, ...configChangeFunctions);
-
-    const extension = isAngular({projectDir}) ? "ts" : "js";
-    const vendorAndroidPath = getFullPath(
-        projectDir,
-        `app/vendor-platform.android.${extension}`
-    );
-
-    editFileContent(vendorAndroidPath, addSnapshotToVendor);
-}
-
-function editFileContent(path, ...funcs) {
-    if (!fs.existsSync(path)) {
-        return;
-    }
-
-    let content = fs.readFileSync(path, "utf8");
-    funcs.forEach(fn => content = fn(content));
-
-    fs.writeFileSync(path, content, "utf8");
-}
-
-function replaceStyleUrlResolvePlugin(config) {
-    if (config.indexOf("StyleUrlResolvePlugin") === -1) {
-        return config;
-    }
-
-    console.info("Replacing deprecated StyleUrlsResolvePlugin with UrlResolvePlugin...");
-    return config.replace(/StyleUrlResolvePlugin/g, "UrlResolvePlugin");
-}
-
-function addSnapshotPlugin(config) {
-    if (config.indexOf("NativeScriptSnapshotPlugin") > -1) {
-        return config;
-    }
-
-    console.info("Adding NativeScriptSnapshotPlugin configuration...");
-    return config.replace(CONFIG_MATCH, CONFIG_REPLACE);
-}
-
-function addSnapshotToVendor(content) {
-    if (content.indexOf("__snapshot") > -1) {
-        return content;
-    }
-
-    console.info("Adding __snapshot configuration to app/vendor-platform.android ...");
-    return content.replace(FRAME_MATCH, SCOPED_FRAME);
-}
-
 function getFullPath(projectDir, filePath) {
     return path.resolve(projectDir, filePath);
 }
@@ -194,6 +108,5 @@ module.exports = {
     addProjectFiles,
     removeProjectFiles,
     forceUpdateProjectFiles,
-    editExistingProjectFiles,
 };
 
