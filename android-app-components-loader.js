d7ef95b14318973a305fff7c39f118ebc8d21e7d
sis0k0 <stanimira.vlaeva@gmail.com>
2018-03-28 17:39:52 +0300

    feat: add loader for registering android app components

--- android-app-components-loader.js
+++ android-app-components-loader.js
@@ -2,7 +2,13 @@ module.exports = function(source) {
     this.cacheable();
     const { modules } = this.query;
     const imports = modules.map(m => `require("${m}");`).join("\n");
-    const augmentedSource = `${imports}\n${source}`;
+    const augmentedSource = `
+        if (!global["__snapshot"]) {
+            ${imports}
+        }
+
+        ${source}
+    `;
 
     this.callback(null, augmentedSource);
 };
