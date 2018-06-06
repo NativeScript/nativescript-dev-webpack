f1cad69f64cae3bd81478528be2c399a3e40f3c6
sis0k0 <stanimira.vlaeva@gmail.com>
2018-06-06 14:19:21 +0300

    refactor(load-app-css): use registerModule instead of require.context

--- load-application-css.js
+++ load-application-css.js
@@ -1,6 +1,9 @@
-const application = require("application");
-require("ui/styling/style-scope");
+const application = require("tns-core-modules/application");
+require("tns-core-modules/ui/styling/style-scope");
+
+global.registerModule("./app.css", () => require("~/app"));
 const appCssContext = require.context("~/", false, /^\.\/app\.(css|scss|less|sass)$/);
 global.registerWebpackModules(appCssContext);
+
 application.loadAppCss();
 
