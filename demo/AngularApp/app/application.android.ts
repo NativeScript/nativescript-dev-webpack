dc38afc52d37e7a08d5e50b30b6397099c36b030
Vasil Chimev <Vasil.Chimev@gmail.com>
2018-06-22 17:47:12 +0300

    refactor(demos): minor tweaks

--- demo/AngularApp/app/application.android.ts
+++ demo/AngularApp/app/application.android.ts
@@ -1,18 +1,10 @@
-// the `JavaProxy` decorator specifies the package and the name for the native *.JAVA file generated.
 @JavaProxy("org.myApp.Application")
 class Application extends android.app.Application {
     onCreate(): void {
         super.onCreate();
-
-        // At this point modules have already been initialized
-
-        // Enter custom initialization code here
     }
 
     protected attachBaseContext(baseContext: android.content.Context) {
         super.attachBaseContext(baseContext);
-
-        // This code enables MultiDex support for the application (if needed)
-        // android.support.multidex.MultiDex.install(this);
     }
 }
--- demo/JavaScriptApp/app/activity.android.js
+++ demo/JavaScriptApp/app/activity.android.js
@@ -1,15 +1,12 @@
-let frame = require("ui/frame");
+const frame = require("ui/frame");
 
-let superProto = android.app.Activity.prototype;
-let Activity = android.app.Activity.extend("org.myApp.MainActivity", {
+const superProto = android.app.Activity.prototype;
+const Activity = android.app.Activity.extend("org.myApp.MainActivity", {
     onCreate: function(savedInstanceState) {
         if(!this._callbacks) {
             frame.setActivityCallbacks(this);
         }
-        // Modules will take care of calling super.onCreate, do not call it here
         this._callbacks.onCreate(this, savedInstanceState, superProto.onCreate);
-
-        // Add custom initialization logic here
     },
     onSaveInstanceState: function(outState) {
         this._callbacks.onSaveInstanceState(this, outState, superProto.onSaveInstanceState);
