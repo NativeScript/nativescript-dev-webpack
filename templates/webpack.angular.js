accb7e30c38511531fcc512d8d4a683121e03191
sis0k0 <stanimira.vlaeva@gmail.com>
2018-07-25 15:50:07 +0300

    fix: define process as undefined
    This will prevent webpack from adding a polyfill for `process` (`process/browser.js`) whenever someone does `typeof process`. What will change:
    
    - process will be undefined instead of Object.
    - `typeof process === 'undefined'` will return true
    - Angular Animations won't assume the app is running in node context
    ([check](https://github.com/angular/angular/blob/d4ac9698ba7dd829600ca4c8129fcbeae9cb4bed/packages/animations/browser/src/render/shared.ts#L17)) and won't trigger logic which breaks {N} animations.
    
    ** Important **
    This may break plugin which check if the `process` object is defined.
    
    fixes https://github.com/NativeScript/nativescript-angular/issues/1433

--- templates/webpack.angular.js
+++ templates/webpack.angular.js
@@ -195,6 +195,7 @@ module.exports = env => {
             // Define useful constants like TNS_WEBPACK
             new webpack.DefinePlugin({
                 "global.TNS_WEBPACK": "true",
+                "process": undefined,
             }),
             // Remove all files from the out dir.
             new CleanWebpackPlugin([ `${dist}/**/*` ]),
