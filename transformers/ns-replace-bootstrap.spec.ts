74e90bca13b4e3fe64f651714571753f81822fa4
sis0k0 <stanimira.vlaeva@gmail.com>
2018-08-22 16:41:28 +0300

    test: add unit tests for bootstrap transformer

---
+++ transformers/ns-replace-bootstrap.spec.ts
@@ -0,0 +1,97 @@
+import { tags } from '@angular-devkit/core';
+import { createTypescriptContext, transformTypescript } from "@ngtools/webpack/src/transformers";
+import { nsReplaceBootstrap } from './ns-replace-bootstrap';
+import { AngularCompilerPlugin } from '@ngtools/webpack';
+
+describe('@ngtools/webpack transformers', () => {
+  describe('replace_bootstrap', () => {
+    it('should replace bootstrap', () => {
+      const input = tags.stripIndent`
+        import { platformNativeScriptDynamic } from "nativescript-angular/platform";
+        import { AppModule } from "./app.module";
+
+        platformNativeScriptDynamic().bootstrapModule(AppModule);
+      `;
+
+      const output = tags.stripIndent`
+        import * as __NgCli_bootstrap_1 from "nativescript-angular/platform-static";
+        import * as __NgCli_bootstrap_2 from "./app/app.module.ngfactory";
+
+        __NgCli_bootstrap_1.platformNativeScript().bootstrapModuleFactory(__NgCli_bootstrap_2.AppModuleNgFactory);
+      `;
+
+      const { program, compilerHost } = createTypescriptContext(input);
+      const ngCompiler = <AngularCompilerPlugin>{
+        typeChecker: program.getTypeChecker(),
+        entryModule: {
+          path: '/project/src/app/app.module', 
+          className: 'AppModule',
+        },
+      };
+      const transformer = nsReplaceBootstrap(() => ngCompiler);
+      const result = transformTypescript(undefined, [transformer], program, compilerHost);
+
+      expect(tags.oneLine`${result}`).toEqual(tags.oneLine`${output}`);
+    });
+
+    it('should replace bootstrap when barrel files are used', () => {
+      const input = tags.stripIndent`
+        import { platformNativeScriptDynamic } from "nativescript-angular/platform";
+        import { AppModule } from './app';
+
+        platformNativeScriptDynamic().bootstrapModule(AppModule);
+      `;
+
+      const output = tags.stripIndent`
+        import * as __NgCli_bootstrap_1 from "nativescript-angular/platform-static";
+        import * as __NgCli_bootstrap_2 from "./app/app.module.ngfactory";
+
+        __NgCli_bootstrap_1.platformNativeScript().bootstrapModuleFactory(__NgCli_bootstrap_2.AppModuleNgFactory);
+      `;
+
+      const { program, compilerHost } = createTypescriptContext(input);
+      const ngCompiler = <AngularCompilerPlugin>{
+        typeChecker: program.getTypeChecker(),
+        entryModule: {
+          path: '/project/src/app/app.module', 
+          className: 'AppModule',
+        },
+      };
+      const transformer = nsReplaceBootstrap(() => ngCompiler);
+      const result = transformTypescript(undefined, [transformer], program, compilerHost);
+
+      expect(tags.oneLine`${result}`).toEqual(tags.oneLine`${output}`);
+    });
+
+    it('should always import platform nativescript first', () => {
+      const input = tags.stripIndent`
+        import { platformNativeScriptDynamic } from "nativescript-angular/platform";
+        import { AppModule } from "./app.module";
+        import "./shared/kinvey.common";
+
+        platformNativeScriptDynamic().bootstrapModule(AppModule);
+      `;
+
+      const output = tags.stripIndent`
+        import * as __NgCli_bootstrap_1 from "nativescript-angular/platform-static";
+        import * as __NgCli_bootstrap_2 from "./app/app.module.ngfactory";
+        import "./shared/kinvey.common";
+
+        __NgCli_bootstrap_1.platformNativeScript().bootstrapModuleFactory(__NgCli_bootstrap_2.AppModuleNgFactory);
+      `;
+
+      const { program, compilerHost } = createTypescriptContext(input);
+      const ngCompiler = <AngularCompilerPlugin>{
+        typeChecker: program.getTypeChecker(),
+        entryModule: {
+          path: '/project/src/app/app.module', 
+          className: 'AppModule',
+        },
+      };
+      const transformer = nsReplaceBootstrap(() => ngCompiler);
+      const result = transformTypescript(undefined, [transformer], program, compilerHost);
+
+      expect(tags.oneLine`${result}`).toEqual(tags.oneLine`${output}`);
+    });
+  });
+});
