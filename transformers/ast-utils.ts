c37e0759969157624994efde837405d278d1be56
sis0k0 <stanimira.vlaeva@gmail.com>
2018-08-22 16:38:11 +0300

    refactor(transformer): import `getFirstNode` from Angular CLI

--- transformers/ast-utils.ts
+++
@@ -1,8 +0,0 @@
-import * as ts from 'typescript';
-
-export function getFirstNode(sourceFile: ts.SourceFile): ts.Node {
-    if (sourceFile.statements.length > 0) {
-        return sourceFile.statements[0];
-    }
-    return sourceFile.getChildAt(0);
-}
