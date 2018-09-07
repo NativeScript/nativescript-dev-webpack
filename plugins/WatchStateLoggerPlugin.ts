e6b96bb7fd39038445b994294dfdc6b82f3679da
sis0k0 <stanimira.vlaeva@gmail.com>
2018-09-07 17:27:44 +0300

    fix: rewrite errored hot update chunk to prevent the HMR process from failing

--- plugins/WatchStateLoggerPlugin.ts
+++ plugins/WatchStateLoggerPlugin.ts
@@ -1,4 +1,5 @@
 import { join } from "path";
+import { writeFileSync } from "fs";
 
 export enum messages {
     compilationComplete = "Webpack compilation complete.",
@@ -24,6 +25,7 @@ export class WatchStateLoggerPlugin {
         });
         compiler.hooks.afterEmit.tapAsync("WatchStateLoggerPlugin", function (compilation, callback) {
             callback();
+
             if (plugin.isRunningWatching) {
                 console.log(messages.startWatching);
             } else {
@@ -32,12 +34,61 @@ export class WatchStateLoggerPlugin {
 
             const emittedFiles = Object
                 .keys(compilation.assets)
-                .filter(assetKey => compilation.assets[assetKey].emitted)
+                .filter(assetKey => compilation.assets[assetKey].emitted);
+
+            if (compilation.errors.length > 0) {
+                WatchStateLoggerPlugin.rewriteHotUpdateChunk(compiler, emittedFiles);
+            }
+
+            // provide fake paths to the {N} CLI - relative to the 'app' folder
+            // in order to trigger the livesync process
+            const emittedFilesFakePaths = emittedFiles
                 .map(file => join(compiler.context, file));
 
             process.send && process.send(messages.compilationComplete, error => null);
             // Send emitted files so they can be LiveSynced if need be
-            process.send && process.send({ emittedFiles }, error => null);
+            process.send && process.send({ emittedFiles: emittedFilesFakePaths }, error => null);
         });
+    }
+
+    /**
+     * Rewrite an errored chunk to make the hot module replace successful.
+     * @param compiler the webpack compiler
+     * @param emittedFiles the emitted files from the current compilation
+     */
+    private static rewriteHotUpdateChunk(compiler, emittedFiles: string[]) {
+        const chunk = this.findHotUpdateChunk(emittedFiles);
+        if (!chunk) {
+            return;
+        }
+
+        const { name } = this.parseHotUpdateChunkName(chunk);
+        if (!name) {
+            return;
+        }
+
+        const absolutePath = join(compiler.outputPath, chunk);
+
+        const newContent = `webpackHotUpdate('${name}', {});`;
+        writeFileSync(absolutePath, newContent);
+    }
+
+    private static findHotUpdateChunk(emittedFiles: string[]) {
+        return emittedFiles.find(file => file.endsWith("hot-update.js"));
+    }
+
+    /**
+     * Parse the filename of the hot update chunk.
+     * @param name bundle.deccb264c01d6d42416c.hot-update.js
+     * @returns { name: string, hash: string } { name: 'bundle', hash: 'deccb264c01d6d42416c' }
+     */
+    private static parseHotUpdateChunkName(name) {
+        const matcher = /^(.+)\.(.+)\.hot-update/gm;
+        const matches = matcher.exec(name);
+
+        return {
+            name: matches[1] || "",
+            hash: matches[2] || "",
+        };
     }
 }
