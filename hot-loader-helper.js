50e1ad70ec2e666f3b739aea0123b995a7e6ee70
Vasil Trifonov <v.trifonov@gmail.com>
2018-09-21 13:34:48 +0300

    refactor: HMR improvements (#667)
    * Add HMR prefix to logs from HMR to be filterable
    
    * add global.__hmrRefresh function so that it can be easily used by users
    
    * Remove timeout as it is handled in the latest modules
    
    * Improve file filtering of emitted hmr files

--- hot-loader-helper.js
+++ hot-loader-helper.js
@@ -2,14 +2,8 @@ module.exports.reload = function(type) { return `
     if (module.hot) {
         module.hot.accept();
         module.hot.dispose(() => {
-            global.__hmrNeedReload = true;
-            setTimeout(() => {
-                if(global.__hmrNeedReload) {
-                    global.__hmrNeedReload = false;
-                    global.__hmrLivesyncBackup();
-                }
-            }, ${type === 'style' ? "global.__hmrInitialSync ? 1000 : 0" : 0});
+            global.__hmrRefresh('${type}');
         })
     }
 `};
-// we need to add a timeout of 1000 if we have a css change, otherwise the app crashes on initial hmr sync
+
