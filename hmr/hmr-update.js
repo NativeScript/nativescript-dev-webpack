fe4abfb548b91b12387c62ee6c168b1bd911214f
Vasil Chimev <vasil.chimev@gmail.com>
2018-12-27 17:43:17 +0200

    feat(Angular): apply changes in application styles at runtime with HMR (#748)
    * feat(Angular): apply changes in application styles at runtime with HMR
    
    * refactor(HMR): unify snippet for different flavors
    
    `global.__hmrInitialSync` is no more used
    `global.__hmrNeedReload` is removed:
    - on changes in multiple files (app.css, main-page.css, main-page.xml,
      main-page.ts) both *reapply styles* and *navigate* logic need to execute
    - this is important for the bootstrap of Angular apps
    
    * refactor(HMR): unify snippet for different flavors
    
    Remove `hmrUpdate()` from `main.ts` as a duplication of `global.__onLiveSync()`.
    `global.__onLiveSync()` ensures all patches get to the bundle.
    
    * fix(HMR): initial updates on app restart

--- hmr/hmr-update.js
+++ hmr/hmr-update.js
@@ -2,5 +2,5 @@ module.exports = () => {
     const update = require("../hot");
     const fileSystemModule = require("tns-core-modules/file-system");
     const applicationFiles = fileSystemModule.knownFolders.currentApp();
-    update(__webpack_require__["h"](), filename => applicationFiles.getFile(filename));
+    return update(__webpack_require__["h"](), filename => applicationFiles.getFile(filename));
 }
