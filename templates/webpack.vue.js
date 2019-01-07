ee4c44eeab40623c91a1b38b58b02e225b9a1cbc
Vasil Chimev <vasil.chimev@gmail.com>
2019-01-07 17:59:37 +0200

    fix(Vue): apply style changes with HMR
    The `bundle-config-loader` executes for `entryPath` with all extensions: `css`, `js`, `ts`, `scss`, etc. Limit only to `js` and `ts`.
    
    Fixes https://github.com/NativeScript/nativescript-dev-webpack/issues/762.

--- templates/webpack.vue.js
+++ templates/webpack.vue.js
@@ -150,7 +150,7 @@ module.exports = env => {
         },
         module: {
             rules: [{
-                    test: entryPath,
+                    test: new RegExp(entryPath + ".(js|ts)"),
                     use: [
                         // Require all Android app components
                         platform === "android" && {
