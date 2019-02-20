694c66bc20601d49e1561424aaed7a5b4212f0cc
Vasil Chimev <vasil.chimev@gmail.com>
2019-02-20 18:34:12 +0200

    fix(HMR): modulePath on Windows to apply changes in app styles at runtime (#807)
    * fix(HMR): modulePath on Windows
    
    Replace backslashes with forward slashes.
    
    * refactor: rename a method

--- markup-hot-loader.js
+++ markup-hot-loader.js
@@ -1,7 +1,9 @@
 const { reload } = require("./hot-loader-helper");
+const { convertToUnixPath } = require("./lib/utils");
 
 module.exports = function (source) {
     const typeMarkup = "markup";
-    const modulePath = this.resourcePath.replace(this.rootContext, ".");
+    const moduleRelativePath = this.resourcePath.replace(this.rootContext, ".");
+    const modulePath = convertToUnixPath(moduleRelativePath);
     return `${source};${reload({ type: typeMarkup, path: modulePath })}`;
 };
