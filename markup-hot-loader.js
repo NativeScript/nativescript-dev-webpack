e0c49fcc0eeb8cd16821fd13d3074ed010f4bfdc
Vasil Chimev <vasil.chimev@gmail.com>
2019-02-20 13:03:38 +0200

    refactor: rename a method

--- markup-hot-loader.js
+++ markup-hot-loader.js
@@ -1,9 +1,9 @@
 const { reload } = require("./hot-loader-helper");
-const { fromRelativePathToUnix } = require("./lib/utils");
+const { convertToUnixPath } = require("./lib/utils");
 
 module.exports = function (source) {
     const typeMarkup = "markup";
     const moduleRelativePath = this.resourcePath.replace(this.rootContext, ".");
-    const modulePath = fromRelativePathToUnix(moduleRelativePath);
+    const modulePath = convertToUnixPath(moduleRelativePath);
     return `${source};${reload({ type: typeMarkup, path: modulePath })}`;
 };
