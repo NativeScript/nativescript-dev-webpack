f305927e2e89d1a1a57524032792b1f6d64938d5
Eduardo Speroni <edusperoni@gmail.com>
2019-02-21 14:49:46 -0300

    fix: sourceMap not generated with Uglify

--- templates/webpack.javascript.js
+++ templates/webpack.javascript.js
@@ -122,6 +122,7 @@ module.exports = env => {
                 new UglifyJsPlugin({
                     parallel: true,
                     cache: true,
+                    sourceMap: !!sourceMap,
                     uglifyOptions: {
                         output: {
                             comments: false,
--- templates/webpack.typescript.js
+++ templates/webpack.typescript.js
@@ -124,6 +124,7 @@ module.exports = env => {
                 new UglifyJsPlugin({
                     parallel: true,
                     cache: true,
+                    sourceMap: !!sourceMap,
                     uglifyOptions: {
                         output: {
                             comments: false,
