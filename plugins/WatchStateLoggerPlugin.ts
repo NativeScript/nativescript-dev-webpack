be82ab7a267d6c8a0ab879fe289115eeae73f8f0
DimitarTachev <nevermindd7@gmail.com>
2019-04-23 11:20:34 +0300

    fix: ignore the Webpack runtime chunks when sending HMR updates

--- plugins/WatchStateLoggerPlugin.ts
+++ plugins/WatchStateLoggerPlugin.ts
@@ -31,6 +31,7 @@ export class WatchStateLoggerPlugin {
                 console.log(messages.compilationComplete);
             }
 
+            const runtimeOnlyFiles = getWebpackRuntimeOnlyFiles(compilation, compiler);
             let emittedFiles = Object
                 .keys(compilation.assets)
                 .filter(assetKey => compilation.assets[assetKey].emitted);
@@ -42,7 +43,28 @@ export class WatchStateLoggerPlugin {
 
             process.send && process.send(messages.compilationComplete, error => null);
             // Send emitted files so they can be LiveSynced if need be
-            process.send && process.send({ emittedFiles: emittedFilesFakePaths }, error => null);
+            process.send && process.send({ emittedFiles: emittedFilesFakePaths, webpackRuntimeFiles: runtimeOnlyFiles }, error => null);
         });
     }
+}
+
+function getWebpackRuntimeOnlyFiles(compilation, compiler) {
+    let runtimeOnlyFiles = [];
+    try {
+        runtimeOnlyFiles = [].concat(...compilation.chunkGroups
+            // get the chunk group of each entry points (e.g. main.js and inspector-modules.js)
+            .map(chunkGroup => chunkGroup.runtimeChunk)
+            // filter embedded runtime chunks (e.g. part of bundle.js or inspector-modules.js)
+            .filter(runtimeChunk => runtimeChunk.preventIntegration)
+            .map(runtimeChunk => runtimeChunk.files))
+            // get only the unique files in case of "single" runtime (e.g. runtime.js)
+            .filter((value, index, self) => self.indexOf(value) === index)
+            // convert to absolute paths
+            .map(fileName => join(compiler.context, fileName));
+    } catch (e) {
+        // breaking change in the Webpack API
+        console.log("Warning: Unable to find Webpack runtime files.");
+    }
+
+    return runtimeOnlyFiles;
 }
