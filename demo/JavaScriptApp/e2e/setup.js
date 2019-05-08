6210292c80fe4fb1cc0f9d4a6f4d4561f492d647
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2019-05-08 10:50:21 +0300

    tests: use mochawesome reporter

--- demo/JavaScriptApp/e2e/setup.js
+++ demo/JavaScriptApp/e2e/setup.js
@@ -1,18 +1,19 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const nativescript_dev_appium_1 = require("nativescript-dev-appium");
-before("start server", () => __awaiter(this, void 0, void 0, function* () {
-    yield nativescript_dev_appium_1.startServer();
-}));
-after("stop server", () => __awaiter(this, void 0, void 0, function* () {
-    yield nativescript_dev_appium_1.stopServer();
-}));
-//# sourceMappingURL=setup.js.map
+const nsAppium = require("nativescript-dev-appium");
+const addContext = require('mochawesome/addContext');
+
+const testReporterContext = {};
+testReporterContext.name = "mochawesome";
+testReporterContext.reportDir = "mochawesome-report";
+testReporterContext.log = addContext;
+testReporterContext.logImageTypes = [nsAppium.LogImageType.screenshots];
+nsAppium.nsCapabilities.testReporter = testReporterContext;
+
+before("start server", async function () {
+    nsAppium.nsCapabilities.testReporter.context = this;
+    await nsAppium.startServer();
+});
+
+after("stop appium server", async function () {
+    nsAppium.nsCapabilities.testReporter.context = this;
+    await nsAppium.stopServer();
+});
