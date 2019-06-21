f0c62fb1ca699ef6b1fa5f11b39674c44e97d4b2
Dimitar Tachev <dimitar.tachev@telerik.com>
2019-06-21 14:49:53 +0300

    fix: follow the symlinks in JavaScript apps (#941)
    We are already following the symlinks in TypeScript, Angular and Vue apps. It allows the plugin developers to link and develop their plugins directly in a JavaScript demo application

--- templates/webpack.javascript.js
+++ templates/webpack.javascript.js
@@ -103,7 +103,7 @@ module.exports = env => {
                 '~': appFullPath
             },
             // don't resolve symlinks to symlinked modules
-            symlinks: false
+            symlinks: true
         },
         resolveLoader: {
             // don't resolve symlinks to symlinked loaders
