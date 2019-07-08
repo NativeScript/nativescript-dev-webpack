f1e808190fa50831b66db369fba9eb153fdcce40
Alexander Vakrilov <alexander.vakrilov@telerik.com>
2019-07-08 13:54:41 +0300

    fix: hmr export default crash (#970)

--- hmr/hmr-update.ts
+++ hmr/hmr-update.ts
@@ -1,4 +1,4 @@
-import update from "../hot";
+import * as hot from "../hot";
 import { knownFolders } from "tns-core-modules/file-system";
 
 declare const __webpack_require__: any;
@@ -6,5 +6,5 @@ declare const __webpack_require__: any;
 export function hmrUpdate() {
     const applicationFiles = knownFolders.currentApp();
     const latestHash = __webpack_require__["h"]();
-    return update(latestHash, filename => applicationFiles.getFile(filename));
+    return hot(latestHash, filename => applicationFiles.getFile(filename));
 }
