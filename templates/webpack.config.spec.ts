7fa376db185157406dae720ca5d9b071271ec52a
vakrilov <alexander.vakrilov@telerik.com>
2019-09-13 11:25:45 +0300

    test: add code coverage with nyc

--- templates/webpack.config.spec.ts
+++ templates/webpack.config.spec.ts
@@ -32,8 +32,12 @@ const nativeScriptDevWebpack = {
     getEntryModule: () => 'EntryModule',
     getResolver: () => null,
     getConvertedExternals: nsWebpackIndex.getConvertedExternals,
-    getSourceMapFilename: nsWebpackIndex.getSourceMapFilename
+    getSourceMapFilename: nsWebpackIndex.getSourceMapFilename,
+    processAppComponents: nsWebpackIndex.processAppComponents,
+    getUserDefinedEntries: nsWebpackIndex.getUserDefinedEntries,
 };
+
+
 
 const emptyObject = {};
 const FakeAotTransformerFlag = "aot";
